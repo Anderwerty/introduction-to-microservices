f25f60b031a253111c6a59737c8e37b1cbbdefc6
andrii_shylin <andrii_shylin@gmail.com>
2025-10-21 01:56:30 +0200

    [task-5] git config server

--- config-server/src/main/java/org/example/ConfigServerApplication.java
+++ config-server/src/main/java/org/example/ConfigServerApplication.java
@@ -3,11 +3,14 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class SongServiceApplication {
+public class ConfigServerApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(SongServiceApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
